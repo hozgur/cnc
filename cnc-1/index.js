483f06991cf7ac9b80f0a024df7f2fe3d89924b3
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2022-03-11 23:07:26 +0300

    tests ok.

--- cnc-1/index.js
+++ cnc-1/index.js
@@ -4,10 +4,12 @@ const yargs = require('yargs');
 
 
 let depth = 0.1;
-let space = 0.1;
+let dx = 0.1;
+let dy = -0.1;
 let feed = 1000;
-let clearance = 0.1; // mm
-let top = 0;
+let clearance = 0.5; // mm
+let spindle = 1000;
+let top_offset = 0;
 let x_offset = 0;
 let y_offset = 0;
 const argv = yargs
@@ -23,9 +25,14 @@ const argv = yargs
     description: 'Depth of the image',
     type: 'number',
   })
-    .option('space', {
-        alias: 's',
-        description: 'Space between the dots',
+    .option('xint', {
+        alias: 'a',
+        description: 'Space between the dots X',
+        type: 'number',
+    })
+    .option('yint', {
+        alias: 'b',
+        description: 'Space between the dots Y',
         type: 'number',
     })
     .option('feed', {
@@ -53,6 +60,11 @@ const argv = yargs
         description: 'Clearance Level for rapid moves',
         type: 'number',
     })
+    .option('spindle', {
+        alias: 'p',
+        description: 'Spindle Speed',
+        type: 'number',
+    })
   .help()
   .alias('help', 'h').argv;
 
@@ -67,11 +79,15 @@ if(argv.feed) {
 }
 
 if(argv.top) {
-    top = argv.top;
+    top_offset = argv.top;
 }
-
 if(argv.clear) {
     clearance = argv.clear;
+}
+
+if(argv.spindle) {
+    spindle = argv.spindle;
+    console.log(argv.spindle);
 }
 
 if(argv.x_offset) {
@@ -81,6 +97,10 @@ if(argv.y_offset) {
     y_offset = argv.y_offset;
 }
 
+if(argv.xint)
+    dx = argv.xint;
+if(argv.yint)
+    dy = argv.yint;
 
 if(argv._.length === 0) {
     console.log('Please provide an image');
@@ -95,26 +115,29 @@ Jimp.read(filename, (err, img) => {
   if (err) throw err;
     img.grayscale();    
     output.write(`G21 G90 G54\n`);
-    output.write(`S2000 M03\n`);
-    const width = img.bitmap.width;
-    const height = img.bitmap.height;
+    output.write(`T7 M6\n`);
+    output.write(`G43 H7\n`);
+    if(spindle > 50)
+        output.write(`S${spindle} M03\n`);
+    let width = img.bitmap.width;
+    let height = img.bitmap.height;
     const data = img.bitmap.data;
     console.log('width:', width);
     console.log('height:', height);
-    console.log('data size:', data.byteLength);
-    
+    console.log('data size:', data.byteLength);    
     for(let y=0; y<height; y++) {
         const i = y*width*4;        
         for(let x=0; x<width; x++) {            
             let val = data[i+x*4];
             if (val < 128) {
-                gotoXY(x_offset+x*space,y_offset+y*space);
+                gotoXY(x_offset+x*dx,y_offset+y*dy);
                 plunge();
             }            
         }
     }
-    output.write(`M05\n`);
-    output.write(`G0 Z${top+15}\n`);
+    if(spindle > 50)
+        output.write(`M05\n`);
+    output.write(`G0 Z${top_offset+25}\n`);
 
 });
 
@@ -132,5 +155,5 @@ function moveXY(x,y) {
 }
 
 function moveZ(z) {
-    output.write(`G1 Z${z+top} F${feed}\n`);
+    output.write(`G1 Z${z+top_offset} F${feed}\n`);
 }
