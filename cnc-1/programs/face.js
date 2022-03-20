3c7229cc3ff82ff9cf7e31b8255bf8ef03a32ad4
Hakan ÖZGÜR <hakan@ait.com.tr>
2022-03-20 15:45:44 +0300

    gcode.js

---
+++ cnc-1/programs/face.js
@@ -0,0 +1,23 @@
+const gcode = require('../gcode.js');
+const tools = require('../tools.js');
+
+const SPINDLE_SPEED = 1000;
+const FEED = 1000;
+const Z_CLEARANCE = 20;
+const Z_FACE = 10;
+console.log(tools.FACE.surfaceSpeed(SPINDLE_SPEED));
+gcode.init_gcode(1001, 'YUZEY TARAMA');
+gcode.tool(tools.FACE.index);
+gcode.G0({Z: Z_CLEARANCE});
+gcode.spindle_CW(SPINDLE_SPEED);
+gcode.coolant_on();
+gcode.G0({X: -30, Y: -25});
+gcode.G0({Z: Z_FACE});
+gcode.G1({X: 30}, FEED);
+gcode.coolant_off();
+gcode.spindle_stop();
+gcode.G0({Z: Z_CLEARANCE});
+gcode.end();
+
+
+
---
+++ cnc-1/tools.js
@@ -0,0 +1,39 @@
+tools = {
+    //
+    FACE : {
+        index : 3,
+        diameter : 50,
+        radius : 25,
+        flutes : 5,
+        surfaceSpeed : function(speed) {
+            return tools.surfaceSpeed(this,speed);
+        }
+    },
+    //
+    CHAMFER : {
+        index : 7,
+        diameter : 8,
+        radius : 4,
+        flutes : 2,
+    },
+    //
+    ENDMILL_16MM : {
+        index : 8,
+        diameter : 16,
+        radius : 8,
+        flutes : 3,
+    },
+    //
+    ENDMILL_6MM : {
+        index : 9,
+        diameter : 6,
+        radius : 3,
+        flutes : 3,
+    },
+    
+    surfaceSpeed : function(tool,spindle_speed) {
+        return 2 * tool.diameter * Math.PI * spindle_speed / 1000;
+    }
+}
+
+module.exports = tools;
