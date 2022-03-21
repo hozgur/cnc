510fdf85462e9e89aa1555f5d03a698392c6aa65
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2022-03-21 15:34:08 +0300

    face implemented

--- cnc-1/programs/face.js
+++ cnc-1/programs/face.js
@@ -1,23 +1,55 @@
 const gcode = require('../gcode.js');
 const tools = require('../tools.js');
 
-const SPINDLE_SPEED = 1000;
+const SPINDLE_SPEED = 4000;
 const FEED = 1000;
-const Z_CLEARANCE = 20;
-const Z_FACE = 10;
-console.log(tools.FACE.surfaceSpeed(SPINDLE_SPEED));
-gcode.init_gcode(1001, 'YUZEY TARAMA');
-gcode.tool(tools.FACE.index);
-gcode.G0({Z: Z_CLEARANCE});
+const Z_CLEARANCE = 90;
+const Z_FACE    = 76.60;
+
+const WIDTH_X   = 69;
+const WIDTH_Y   = 9.7;
+const tool_diameter = tools.FACE.diameter;
+
+const stock_x = 50;
+const stock_y = 10;
+const delta_y = (tool_diameter-5)>0?(tool_diameter-5):tool_diameter;
+
+const start_x = stock_x - tool_diameter - 5;
+const start_y = stock_y -tool_diameter/2;
+
+const end_x = stock_x + WIDTH_X + tool_diameter/2 + 5;
+
+const pass_count = Math.floor(WIDTH_Y/(delta_y)+0.99);
+console.log(`pass_count: ${pass_count}`);
+const surface_speed = tools.FACE.surfaceSpeed(SPINDLE_SPEED);
+console.log(`surface_speed: ${surface_speed}`);
+
+
+// Sıfırlamalar
+gcode.init_gcode(1003, 'YUZEY TARAMA');
+gcode.text(`pass_count: ${pass_count}`);
+gcode.text(`surface_speed: ${surface_speed}`);
+gcode.text(`tool_diameter: ${tool_diameter}`);
+gcode.text(`Z_CLEARANCE: ${Z_CLEARANCE}`);
+
+// Tool seçme
+gcode.tool(tools.FACE.index,Z_CLEARANCE);
+
+// Spindle açma
 gcode.spindle_CW(SPINDLE_SPEED);
+// Soğutma açma
 gcode.coolant_on();
-gcode.G0({X: -30, Y: -25});
-gcode.G0({Z: Z_FACE});
-gcode.G1({X: 30}, FEED);
+
+for(let y=0; y<pass_count; y++) {
+    gcode.text(`Pass: ${y+1}`);
+    const y_pos = start_y - y*delta_y;
+    gcode.G0({X: start_x, Y: y_pos});
+    gcode.G0({Z: Z_FACE});
+    gcode.G1({X: end_x, F: FEED});
+    gcode.G0({Z: Z_CLEARANCE});
+}
 gcode.coolant_off();
-gcode.spindle_stop();
-gcode.G0({Z: Z_CLEARANCE});
+gcode.relative_mode();
+gcode.G0({Z: 100});
+gcode.absolute_mode();
 gcode.end();
-
-
-
